35e460829ab64597d90e2471bdea54bf11f51383
neiterman21 <neiterman21@gmail.com>
2023-05-03 16:29:16 +0300

    fixed issues

--- Demo.cpp
+++ Demo.cpp
@@ -20,32 +20,32 @@ using namespace ariel;
 int main() {
     Point a(32.3,44),b(1.3,3.5);
     assert(a.distance(b) == b.distance(a));
-    Cowboy tom = new Cowboy("Tom", a);
-    OldNinja sushi = new OldNinja("sushi", b);
-    tom.shoot(sushi);
-    cout << tom->Print() <<endl;
+    Cowboy *tom = new Cowboy("Tom", a);
+    OldNinja *sushi = new OldNinja("sushi", b);
+    tom->shoot(sushi);
+    cout << tom->print() <<endl;
 
-    sushi.move(tom);
-    sushi.slash(tom);
+    sushi->move(tom);
+    sushi->slash(tom);
 
-    Team a(tom); 
-    a.add(new YountNinja("Yogi", Point(64,57)));
+    Team team_A(tom); 
+    team_A.add(new YountNinja("Yogi", Point(64,57)));
 
-    // Team b(&tom); should throw tom is already in team a
+    // Team b(tom); should throw tom is already in team a
 
-     Team b(&sushi);
-     b.add(new TrainedNinja("Hikari", Point(12,81)));
+     Team team_B(sushi);
+     team_B.add(new TrainedNinja("Hikari", Point(12,81)));
 
 
-     while(a.stillAlive() > 0 && b.stillAlive() > 0){
-        a.attack(&b);
-        b.attack(&a);
-        a.print();
-        b.print();
+     while(team_A.stillAlive() > 0 && team_B.stillAlive() > 0){
+        team_A.attack(&team_B);
+        team_B.attack(&team_A);
+        team_A.print();
+        team_B.print();
      }
 
-     if (a.stillAlive() > 0) cout << "winner is a" << endl;
-     else cout << "winner is b" << endl;
+     if (team_A.stillAlive() > 0) cout << "winner is team_A" << endl;
+     else cout << "winner is team_B" << endl;
 
      return 0; // no memory issues. Team should free the memory of its members. both a and b teams are on the stack. 
 
